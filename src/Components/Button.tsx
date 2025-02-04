8e839661914943d99a72084a5c57f25cbc05c86e
Nishanth <nishanth1596@gmail.com>
2025-02-04 15:15:03 +0530

    added hover color

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -7,12 +7,12 @@ type ButtonProps = {
 
 const Button = ({ type, color, spanColor, isFooter }: ButtonProps) => {
   const base =
-    "text-White rounded-4xl text-base leading-6  font-black px-10 py-4";
+    "text-White rounded-4xl text-base leading-6  font-black px-10 py-4 hover:cursor-pointer";
 
   if (type === "download") {
     return (
       <button
-        className={`bg-Cyan-600 ${base} mx-14 inline-block md:mr-0 md:ml-auto`}
+        className={`bg-Cyan-600 hover:bg-Cyan-hover ${base} mx-14 inline-block md:mr-0 md:ml-auto`}
         style={{ backgroundColor: color, margin: isFooter ? "0" : undefined }}
       >
         Download{" "}
@@ -26,7 +26,7 @@ const Button = ({ type, color, spanColor, isFooter }: ButtonProps) => {
   if (type == "what") {
     return (
       <button
-        className={`${base} bg-Purple-600 mx-[4.5rem] inline-block md:mr-auto md:ml-0`}
+        className={`${base} bg-Purple-600 hover:bg-Purple-hover mx-[4.5rem] inline-block md:mr-auto md:ml-0`}
       >
         What is it?
       </button>
