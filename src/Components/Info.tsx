70d1004778de082f67cad2da17532bfcfcb259d3
Nishanth <nishanth1596@gmail.com>
2025-02-04 15:53:30 +0530

    fixed alignemt issue in the info section

--- src/Components/Info.tsx
+++ src/Components/Info.tsx
@@ -23,11 +23,11 @@ const Info = () => {
       <p className="text-Cyan-600 mt-16 text-xs leading-[1.1] font-black tracking-[4px] md:text-sm lg:text-base">
         Built For Modern Use
       </p>
-      <h2 className="text-Slate-900 mt-4 text-[2rem] leading-[1.1] font-black md:mx-[7.47rem] md:text-4xl lg:mx-[23.7rem] lg:text-[2.5rem]">
+      <h2 className="text-Slate-900 mt-4 text-[2rem] leading-[1.1] font-black md:mx-[7.47rem] md:text-4xl lg:mx-[29.1rem] lg:text-[2.5rem]">
         Smarter meetings, all in one place
       </h2>
 
-      <p className="text-Slate-600 mt-8 text-base leading-6 font-medium md:mx-[5.63rem] lg:mx-[20.6rem] lg:text-lg">
+      <p className="text-Slate-600 mt-8 text-base leading-6 font-medium md:mx-[5.63rem] lg:mx-[26rem] lg:text-lg">
         Send messages, share files, show your screen, and record your meetings -
         all in one workspace. Control who can join with invite-only team access,
         data encryption, and data export.
